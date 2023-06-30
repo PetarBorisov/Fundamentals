6c0b7bd47ac03145ae3f948c2b3bd029ef5c6809
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-06-30 22:44:36 +0300

    Create Horse.java

---
+++ DemoPet/Horse.java
@@ -0,0 +1,10 @@
+package DemoPet;
+
+public class Horse extends Demo{
+
+    public static final int HORSE_AGE = 25;
+    public static final double HORSE_KG = 400;
+    public Horse(String dog, String cat, String horse) {
+        super(dog, cat, horse, HORSE_AGE, HORSE_KG);
+    }
+}
