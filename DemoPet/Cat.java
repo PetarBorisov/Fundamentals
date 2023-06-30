ff8560a5d1b48a5677496182651e22ede2879265
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-06-30 22:44:27 +0300

    Create Cat.java

---
+++ DemoPet/Cat.java
@@ -0,0 +1,9 @@
+package DemoPet;
+
+public class Cat extends Demo{
+    public static final int CAT_AGE = 10;
+    public static final double CAT_KG = 5.5;
+    public Cat(String dog, String cat, String horse) {
+        super(dog, cat, horse, CAT_AGE,CAT_KG);
+    }
+}
