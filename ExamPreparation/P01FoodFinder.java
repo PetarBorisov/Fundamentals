b45b7f26b9932cf06980df441ca5d95cfe33714c
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-08-24 00:28:35 +0300

    Create P01FoodFinder.java

---
+++ ExamPreparation/P01FoodFinder.java
@@ -0,0 +1,48 @@
+package ExamPreparation;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class P01FoodFinder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] words = new String[]{"pear", "flour", "pork", "olive"};
+        String[] foundWords = new String[]{"****", "*****", "****", "*****"};
+
+        ArrayDeque<Character> queueVowels = new ArrayDeque<>();
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                .forEach(e -> queueVowels.offer(e.charAt(0)));
+
+        ArrayDeque<Character> stackConsonants = new ArrayDeque<>();
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                .forEach(e -> stackConsonants.push(e.charAt(0)));
+
+        while (!stackConsonants.isEmpty() && !queueVowels.isEmpty()) {
+            char vowel = queueVowels.poll();
+            char consonant = stackConsonants.pop();
+
+            for (int i = 0; i < words.length; i++) {
+                String word = words[i];
+                int indexVowel = word.indexOf(vowel);
+                int indexConsonant = word.indexOf(consonant);
+                if (indexVowel >= 0) {
+                    foundWords[i] = foundWords[i].substring(0, indexVowel) + vowel + foundWords[i].substring(indexVowel + 1);
+                }
+                if (indexConsonant >= 0) {
+                    foundWords[i] = foundWords[i].substring(0, indexConsonant) + consonant + foundWords[i].substring(indexConsonant + 1);
+                }
+            }
+                queueVowels.offer(vowel);
+            }
+
+            List<String> output = Arrays.stream(foundWords).filter(w -> !w.contains("*")).collect(Collectors.toList());
+
+            System.out.printf("Words found: %d%n", output.size());
+            output.forEach(System.out::println);
+        }
+
+    }
