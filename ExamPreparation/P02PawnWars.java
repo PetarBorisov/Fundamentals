2865b1fc16f9dcad49a7ddaf8843d6c64b9d9a50
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-08-24 00:28:48 +0300

    Create P02PawnWars.java

---
+++ ExamPreparation/P02PawnWars.java
@@ -0,0 +1,11 @@
+package ExamPreparation;
+
+import java.util.Scanner;
+
+public class P02PawnWars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
