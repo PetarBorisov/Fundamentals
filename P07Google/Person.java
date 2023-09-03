2e7a0c99dc581b7425f5f0e3212b9304a825c63a
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-09-03 11:13:19 +0300

    Create Person.java

---
+++ P07Google/Person.java
@@ -0,0 +1,50 @@
+package P07Google;
+
+import java.util.Map;
+
+public class Person {
+    private String name;
+
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+
+
+    }
+
+    public void setCompany(Company newCompany) {
+
+    }
+
+    public void setPokemon(Pokemon newPokemon) {
+
+    }
+
+    public void setParents(Parents newParents) {
+
+    }
+
+    public void setChildren(Children newChildren) {
+
+    }
+
+    public void setCar(Car newCar) {
+
+    }
+
+    public Company getCompany() {
+        return null;
+    }
+
+    public Car getCar() {
+        return null;
+    }
+}
