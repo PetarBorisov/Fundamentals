7749698123ad3881d6f64cdcb09cd843f3108960
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-09-06 01:35:36 +0300

    Create Workout.java

---
+++ Workouts/Workout.java
@@ -0,0 +1,81 @@
+package Workouts;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Workout {
+    private String type;
+
+
+    private int exerciseCount;
+    List<Exercise> exercises;
+
+    public Workout(String type, int exerciseCount) {
+        this.type = type;
+        this.exerciseCount = exerciseCount;
+        this.exercises = new ArrayList<>();
+    }
+
+    public void addExercise(Exercise exercise) {
+        if (this.exercises.size() < exerciseCount) {
+            this.exercises.add(exercise);
+        }
+    }
+
+    public boolean removeExercise(String name, String muscle) {
+        for (Exercise exercise : this.exercises) {
+            if (exercise.getName().equals(name) && exercise.getMuscle().equals(muscle)) {
+                this.exercises.remove(exercise);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Exercise getExercise(String name, String muscle) {
+        for (Exercise datum : this.exercises) {
+            if (datum.getName().equals(name) && datum.getMuscle().equals(muscle)) {
+                return datum;
+            }
+        }
+        return null;
+    }
+
+    public Exercise getMostBurnedCaloriesExercise() {
+        Exercise exercise = null;
+        int calories = 0;
+
+        if (this.exercises.size() > 0) {
+            for (Exercise ex : this.exercises) {
+                if (ex.getBurnedCalories() > calories) {
+                    calories = ex.getBurnedCalories();
+                }
+            }
+            for (Exercise ex : this.exercises) {
+                if (ex.getBurnedCalories() == calories) {
+                    exercise = ex;
+                    break;
+                }
+            }
+        }
+        return exercise;
+    }
+
+    public int getExerciseCount() {
+        return exerciseCount;
+    }
+
+
+    public String getStatistics() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(String.format("Workout type: %s", this.type));
+        builder.append(System.lineSeparator());
+
+        for (Exercise exercise : this.exercises) {
+            builder.append(exercise);
+            builder.append(System.lineSeparator());
+        }
+
+        return builder.toString().trim();
+    }
+}
